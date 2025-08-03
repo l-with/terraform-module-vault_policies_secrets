6a76d5aac62adb0d0ae31c4934ec2d3b0ec290bc
Lars With <lars@with.de>
2025-08-03 19:55:15 +0200

    vault provider 5.1

--- versions.tf
+++ versions.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.3"
   required_providers {
     vault = {
       source  = "hashicorp/vault"
-      version = ">= 3.8"
+      version = "~> 5.1"
     }
   }
 }
